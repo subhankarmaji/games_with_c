df2b37ef1d045cad8f9c19e1e36f5768c26e2840
Subhankar maji <49182085+subhankarmaji@users.noreply.github.com>
2020-10-08 20:47:30 +0530

    Update TikTakToe.c

--- TikTakToe.c
+++ TikTakToe.c
@@ -122,7 +122,7 @@ void board()
     printf("_____|_____|_____\n");
     printf("     |     |     \n");
 
-
+    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
 
     printf("_____|_____|_____\n");
     printf("     |     |     \n");
